6b3b3055219aa323474704872698c5c9217820ee
bensmus <bensmus@gmail.com>
2021-05-23 02:58:42 -0700

    update all percents

--- calculator.js
+++ calculator.js
@@ -8,16 +8,32 @@
 // }
 
 // Displaying the percentage for first row
-let p1 = document.getElementById("p1");
-let n1 = document.getElementById("n1");
-let d1 = document.getElementById("d1");
+// let p1 = document.getElementById("p1");
+// let n1 = document.getElementById("n1");
+// let d1 = document.getElementById("d1");
 
-n1.oninput = updatePercent;
-d1.oninput = updatePercent;
+// n1.oninput = updatePercent;
+// d1.oninput = updatePercent;
 
-function updatePercent() {
-    p1.innerHTML = Math.round(n1.value / d1.value * 100) + "%";
+// function updatePercent() {
+//     p1.innerHTML = Math.round(n1.value / d1.value * 100) + "%";
+// }
+
+// Displaying percentages for all rows
+for (let count = 1; count <= 4; count++) {
+    let p = document.getElementById("p" + count)
+    let n = document.getElementById("n" + count);
+    let d = document.getElementById("d" + count);
+
+    n.oninput = function () {
+        p.innerHTML = Math.round(n.value / d.value * 100) + "%";
+    };
+
+    d.oninput = function () {
+        p.innerHTML = Math.round(n.value / d.value * 100) + "%";
+    };
 }
+
 
 
 
