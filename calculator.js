be71e786d8bad8fa800bc24a988531ce32d65a7a
bensmus <bensmus@gmail.com>
2021-05-23 01:21:22 -0700

    learning js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,9 @@
-console.log(document.getElementsByTagName("input"))
+// Testing "displaying something based on certain conditions"
+let n = Math.round(Math.random())
+var p1 = document.getElementById("p1")
+if (n == 1) {
+    p1.innerHTML = "1";
+} else {
+    p1.innerHTML = "0";
+}
+
