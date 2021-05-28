73c021f804dfce8dea1242c7d95175c66ac39d7b
bensmus <bensmus@gmail.com>
2021-05-28 12:38:56 -0700

    refactor percent creation

--- calculator.js
+++ calculator.js
@@ -1,30 +1,13 @@
-// // Testing "displaying something based on certain conditions"
-// let n = Math.round(Math.random())
-// let p1 = document.getElementById("p1")
-// if (n == 1) {
-//     p1.innerHTML = "1";
-// } else {
-//     p1.innerHTML = "0";
-// }
-
-// Displaying the percentage for first row
-// let p1 = document.getElementById("p1");
-// let n1 = document.getElementById("n1");
-// let d1 = document.getElementById("d1");
-
-// n1.oninput = updatePercent;
-// d1.oninput = updatePercent;
-
-// function updatePercent() {
-//     p1.innerHTML = Math.round(n1.value / d1.value * 100) + "%";
-// }
-
 let gradeFractions = {}
 let weights = {}
 let rowcount = 4;
 
 // Displaying percentages for all rows
 for (let count = 1; count <= rowcount; count++) {
+    percentInitialize(count);
+}
+
+function percentInitialize(count) {
     let p = document.getElementById("p" + count)
     let n = document.getElementById("n" + count);
     let d = document.getElementById("d" + count);
