8f58b14945880411f7964c55a115d8ad8702aee0
bensmus <bensmus@gmail.com>
2021-05-31 23:03:08 -0700

    too many warnings

--- calculator.js
+++ calculator.js
@@ -1,6 +1,8 @@
-let gradeFractions = {}
-let weights = {}
+let gradeFractions = {};
+let weights = {};
 let rowtotal = 4;
+let weightSheet = document.styleSheets[0];
+let tooltip = document.getElementById("tooltip");
 
 // Displaying percentages for all rows
 for (let rowcount = 1; rowcount <= rowtotal; rowcount++) {
@@ -16,7 +18,8 @@ function initialize(rowcount) {
     percentUpdateFunction = percentUpdate(n, d, p, rowcount);
     weightUpdateFunction();
     percentUpdateFunction();
-    w.oninput = weightUpdateFunction;
+    w.addEventListener("input", weightUpdateFunction);
+    w.addEventListener("input", updateWarning(weights));
     n.oninput = percentUpdateFunction;
     d.oninput = percentUpdateFunction;
 }
@@ -118,4 +121,25 @@ rowMinus.onclick = function () {
         rowToRemove.remove();
         rowtotal--;
     }
+}
+
+function updateWarning(weights) {
+    return function () {
+        if (sumWeights(weights) != "1.00" && weightSheet.cssRules.length == 1) {
+            weightSheet.insertRule('input.weight { background-image: url("resources/warning.png"); }', weightSheet.cssRules.length);
+            tooltip.style.visibility = "visible";
+        } if (sumWeights(weights) == "1.00" && weightSheet.cssRules.length == 2) {
+            weightSheet.deleteRule(weightSheet.cssRules.length - 1);
+            tooltip.style.visibility = "hidden";
+        }
+    }
+
+}
+
+function sumWeights(weights) {
+    let sum = 0;
+    for (let key in weights) {
+        sum += weights[key];
+    }
+    return sum.toPrecision(3);
 }
